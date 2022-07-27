3fc3ec5c51d6328fc09c684a7e32626f432f6927
gathoni-k <maryg.kahiga@gmail.com>
2022-07-27 11:29:38 +0300

    Create post page

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -2,12 +2,20 @@ import React from "react";
 import { getMDXComponent } from "mdx-bundler/client";
 import { getAllPosts, getSinglePost } from "../../utils/mdx";
 import styles from "../../styles/Home.module.css";
+import Navbar from "../../components/Navbar/Navbar"
 const Post = ({ code, frontmatter }) => {
   const Component = React.useMemo(() => getMDXComponent(code), [code]);
 
   return (
     <div className={styles.container}>
-      <Component />
+      <Navbar/>
+      <div className={styles.post}>
+          <h1 className={styles.title}>{frontmatter.title}</h1>
+          <span className={styles.date}>Mary Gathoni / June 13, 2022</span>
+          <p className={styles.category}>[{frontmatter.category}]</p>
+          <Component />
+      </div>
+    
     </div>
   );
 };
