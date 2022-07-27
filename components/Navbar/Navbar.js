8ebc54f8b6bc8867e6667a6df9eaeeebda49d88d
gathoni-k <maryg.kahiga@gmail.com>
2022-07-27 11:46:56 +0300

    Add links to menu

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import { HiOutlineMenuAlt4 } from "react-icons/hi";
 import { BsSun } from "react-icons/bs";
 import HamburgerMenu from "./Hamburger";
 import { useState } from "react";
+import Link from "next/link";
 export default function Navbar() {
     const [open, setopen] = useState(false)
     const handleClick = () => {
@@ -15,9 +16,15 @@ export default function Navbar() {
                 <HamburgerMenu  isOpen={open} menuClicked={handleClick} width={14} height={8} strokeWidth={1.3} />
             </div>
             <ul className={styles.menuitems}>
-                <li>Home</li>
-                <li>Posts</li>
-                <li>Snippets</li>
+                <Link href="/" passHref>
+                    <a className={styles.linkitems}>Home</a>
+                    </Link>
+                <Link href="/" passHref>
+                <a className={styles.linkitems}>Posts</a>
+                </Link>
+                <Link href="/" passHref>
+                <a className={styles.linkitems}>Snippets</a>
+                </Link>
             </ul>
             <ul className={styles.theme}>
                 <BsSun className={styles.themeicon}/>
@@ -25,9 +32,15 @@ export default function Navbar() {
         </div>
         <div className={open?styles.sidenav:styles.sidenavClosed}>
         <ul className={styles.sideitems}>
-                <li>Home</li>
-                <li>Posts</li>
-                <li>Snippets</li>
+        <Link href="/" passHref>
+                    <a className={styles.sidelinks}>Home</a>
+                    </Link>
+                <Link href="/" passHref>
+                <a className={styles.sidelinks}>Posts</a>
+                </Link>
+                <Link href="/" passHref>
+                <a className={styles.sidelinks}>Snippets</a>
+                </Link>
             </ul>
         
         </div>
