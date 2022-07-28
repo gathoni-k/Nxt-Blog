595de3e21f6ca39f253be0f59db8f843fe0272b6
gathoni-k <maryg.kahiga@gmail.com>
2022-07-28 08:19:02 +0300

    Add active links

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import { BsMoon, BsSun } from "react-icons/bs";
 import HamburgerMenu from "./Hamburger";
 import { useState } from "react";
 import Link from "next/link";
+import ActiveLink from "../ActiveLink/ActiveLink";
 export default function Navbar() {
     const [open, setopen] = useState(false)
     const [theme, settheme] = useState("light")
@@ -20,15 +21,15 @@ export default function Navbar() {
                 <HamburgerMenu  isOpen={open} menuClicked={handleClick} width={14} height={8} strokeWidth={1.3} />
             </div>
             <ul className={styles.menuitems}>
-                <Link href="/" passHref>
-                    <a className={styles.linkitems}>Home</a>
-                    </Link>
-                <Link href="/" passHref>
-                <a className={styles.linkitems}>Posts</a>
-                </Link>
-                <Link href="/" passHref>
-                <a className={styles.linkitems}>Snippets</a>
-                </Link>
+            <ActiveLink activeClassName={styles.active} href="/" passHref>
+                <a className={styles.sidelinks}>Home</a>
+            </ActiveLink>
+                <ActiveLink activeClassName={styles.active} href="/posts" passHref>
+                <a className={styles.sidelinks}>Posts</a>
+                </ActiveLink>
+                <ActiveLink activeClassName={styles.active} href="/snippets" passHref>
+                <a className={styles.sidelinks}>Snippets</a>
+                </ActiveLink>
             </ul>
             <ul className={styles.theme} onClick={switchTheme}>
                 {theme== "light"?<BsSun className={styles.themeicon}/>:<BsMoon className={styles.themeicon}/>}
@@ -36,14 +37,14 @@ export default function Navbar() {
         </div>
         <div className={open?styles.sidenav:styles.sidenavClosed}>
         <ul className={styles.sideitems}>
-        <Link href="/" passHref>
-                    <a className={styles.sidelinks}>Home</a>
-                    </Link>
                 <Link href="/" passHref>
-                <a className={styles.sidelinks}>Posts</a>
+                    <a className={styles.sidelinks}>Home</a>
                 </Link>
                 <Link href="/" passHref>
-                <a className={styles.sidelinks}>Snippets</a>
+                    <a className={styles.sidelinks}>Posts</a>
+                </Link>
+                <Link href="/" passHref>
+                    <a className={styles.sidelinks}>Snippets</a>
                 </Link>
             </ul>
         
